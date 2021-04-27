5d00e9ac392dda901c7b05d6d4b10e56d6b0f3bd
kuri0726 <82706716+kuri0726@users.noreply.github.com>
2021-04-27 16:28:17 +0900

    Update pandas_1.py

--- pandas_1.py
+++ pandas_1.py
@@ -3,11 +3,11 @@ import numpy as np
 import pandas as pd
 
 #データフレーム表示用関数
-from Ipython.display import display
+from IPython.display import display
 #データフレームでの表示制度　小数点4桁まで表示
-pd.opitions.display.float_format = '{:,4f}'.format
+pd.options.display.float_format = '{:.4f}'.format
 #データフレームですべての項目を表示
-pd.set_opition("display.max_columns",None)
+pd.set_option("display.max_columns",None)
 
 b = np.array([[1,2,1],[4,5,2],[7,8,2],[10,np.nan,1],[13,10,2]])
 print(b)

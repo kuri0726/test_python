b86454b3d499a1eee13f14aa60c756046c5e9208
kuri0726 <82706716+kuri0726@users.noreply.github.com>
2021-04-27 15:22:54 +0900

    Create pandas_1.py

---
+++ pandas_1.py
@@ -0,0 +1,21 @@
+#pandas パンダス
+import numpy as np
+import pandas as pd
+
+#データフレーム表示用関数
+from Iputhon.display import display
+#データフレームでの表示制度　小数点4桁まで表示
+pd.opitions.display.float_format = '{:,4f}'.format
+#データフレームですべての項目を表示
+pd.set_opition("display.max_columns",None)
+
+b = np.array([[1,2,1],[4,5,2],[7,8,2],[10,np.nan,1],[13,10,2]])
+print(b)
+
+#データフレームの定義
+df = pd.DataFrame(b, columns=['col_a','col_b','col_c'])
+
+#型表示
+print(type(df))
+#display関数による整形表示
+display(df)

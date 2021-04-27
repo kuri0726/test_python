9d455a55d5e6ddf94ab11ab0614fb019b54b997a
kuri0726 <82706716+kuri0726@users.noreply.github.com>
2021-04-27 16:42:09 +0900

    Update pandas_2.py

--- pandas_2.py
+++ pandas_2.py
@@ -1,6 +1,18 @@
 
 # CSVファイルからの読み込み
 
+import numpy as np
+import pandas as pd
+
+#データフレーム表示用関数
+from IPython.display import display
+#データフレームでの表示制度　小数点4桁まで表示
+pd.options.display.float_format = '{:.4f}'.format
+#データフレームですべての項目を表示
+pd.set_option("display.max_columns",None)
+
+
+
 # 読み込み元URL
 csv_url = 'https://github.com/makaishi2/sample-data\
 /raw/d2b5d7e7c3444d995a1fed5bdadf703709946c75/data/df-sample.csv'

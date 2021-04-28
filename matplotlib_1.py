bf1e156d5be7900f5df7df5bc4d35b69017ae202
kuri0726 <82706716+kuri0726@users.noreply.github.com>
2021-04-28 23:42:43 +0900

    Update matplotlib_1.py

--- matplotlib_1.py
+++ matplotlib_1.py
@@ -32,30 +32,11 @@ xs = df_iris['sepal_length']
 # 散布図y座標用Series
 ys = df_iris['sepal_width']
 
-
 # サイズ設定
 plt.rcParams['figure.figsize'] = (6, 6)
 
 # 散布図
 plt.scatter(xs, ys)
-
-# 描画
-plt.show()
-
-# データ準備
-
-# シグモイド関数の定義
-def sigmoid(x, a):
-    return 1/(1 + np.exp(-a*x))
-
-# グラフ描画用x座標リスト
-xp = np.linspace(-3, 3, 61)
-
-# サイズ設定
-plt.rcParams['figure.figsize'] = (6, 6)
-
-# グラフ描画
-plt.plot(xp, sigmoid(xp, 1.0))
 
 # 描画
 plt.show()
